df9f5b96c7f8d61178a1aa1a6d0e929f6f82112b
BNDamon <odamonoo@gmail.com>
2025-11-02 13:23:55 -0800

    mern environment

---
+++ client/mern-stack/backend/server.js
@@ -0,0 +1,13 @@
+import 'dotenv/config.js';
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js"; // make sure it is db.js and not just db
+connectDB()
+dotenv.config();
+const app = express();
+app.get("/products",(req, res) => { 
+res.send("Server is ready");
+});
+app.listen(5000, () => {
+console.log("Server started at http://localhost:5000");
+});
